fb13b5147443c2ebb48a6a40583209793acee448
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-10-22 12:33:16 +0900

    Update unit tests.

--- app/src/test/java/com/quangnguyen/stackoverflowclient/ui/QuestionsPresenterTest.java
+++ app/src/test/java/com/quangnguyen/stackoverflowclient/ui/QuestionsPresenterTest.java
@@ -10,6 +10,7 @@ import io.reactivex.Scheduler;
 import io.reactivex.schedulers.TestScheduler;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +24,7 @@ import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 /**
  * @author QuangNguyen (quangctkm9207).
@@ -57,14 +59,23 @@ public class QuestionsPresenterTest {
     computationScheduler = testScheduler;
     uiScheduler = testScheduler;
     presenter = new QuestionsPresenter(repository, view, computationScheduler, uiScheduler);
+
+    // Update stub
+    question1.setTitle("question1");
+    question2.setTitle("question2");
+    question3.setTitle("question3");
   }
 
   @Test
   public void loadQuestions_FromRepoToView_WithDataReturned() {
+    // Given
     doReturn(Flowable.just(questions)).when(repository).loadQuestions(true);
+
+    // When
     presenter.loadQuestions(true);
     testScheduler.triggerActions(); // Trigger actions for test scheduler
 
+    // Then
     verify(view).clearQuestions();
     verify(view).showQuestions(questions);
     verify(view, atLeastOnce()).stopLoadingIndicator();
@@ -72,13 +83,47 @@ public class QuestionsPresenterTest {
 
   @Test
   public void loadQuestions_FromRepoToView_WithNoDataReturned() {
+    // Given
     doReturn(Flowable.just(new ArrayList<Question>())).when(repository).loadQuestions(true);
+
+    // When
     presenter.loadQuestions(true);
     testScheduler.triggerActions(); // Trigger actions for test scheduler
 
+    // Then
     verify(view).clearQuestions();
     verify(view, never()).showQuestions(questions);
     verify(view).showNoDataMessage();
     verify(view, atLeastOnce()).stopLoadingIndicator();
+  }
+
+  @Test
+  public void search_ResultShouldBeShownOnView_WhenFilteredDataIsNotEmpty() {
+    // Given
+    doReturn(Flowable.just(questions)).when(repository).loadQuestions(false);
+
+    // When
+    presenter.search("question1");
+    testScheduler.triggerActions();
+
+    // Then
+    // Return a list of questions which should contains only question 1.
+    verify(view).showQuestions(Collections.singletonList(question1));
+    verifyNoMoreInteractions(view);
+  }
+
+  @Test
+  public void search_EmptyMessageShouldBeShownOnView_WhenFilteredDataIsEmpty() {
+    // Given
+    doReturn(Flowable.just(questions)).when(repository).loadQuestions(false);
+
+    // When
+    presenter.search("invalid question title");
+    testScheduler.triggerActions();
+
+    // Then
+    verify(view).clearQuestions();
+    verify(view).showEmptySearchResult();
+    verifyNoMoreInteractions(view);
   }
 }
