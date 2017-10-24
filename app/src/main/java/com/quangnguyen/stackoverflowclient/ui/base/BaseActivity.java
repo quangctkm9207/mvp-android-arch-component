84c7acaeb9299cca4980a454957b1d094e07e360
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-10-24 13:26:00 +0900

    Remove deprecated LifecycleRegistryOwner.

--- app/src/main/java/com/quangnguyen/stackoverflowclient/ui/base/BaseActivity.java
+++ app/src/main/java/com/quangnguyen/stackoverflowclient/ui/base/BaseActivity.java
@@ -1,9 +1,6 @@
 package com.quangnguyen.stackoverflowclient.ui.base;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.arch.lifecycle.LifecycleRegistry;
-import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.support.v7.app.AppCompatActivity;
 import com.quangnguyen.stackoverflowclient.AndroidApplication;
 import com.quangnguyen.stackoverflowclient.data.QuestionRepositoryComponent;
@@ -11,14 +8,8 @@ import com.quangnguyen.stackoverflowclient.data.QuestionRepositoryComponent;
 /**
  * @author QuangNguyen (quangctkm9207).
  */
-public class BaseActivity extends AppCompatActivity implements LifecycleRegistryOwner {
+public class BaseActivity extends AppCompatActivity {
   private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
-
-  protected void addFragment(int containerViewId, Fragment fragment) {
-    final FragmentTransaction fragmentTransaction = this.getFragmentManager().beginTransaction();
-    fragmentTransaction.add(containerViewId, fragment);
-    fragmentTransaction.commit();
-  }
 
   protected QuestionRepositoryComponent getQuestionRepositoryComponent() {
     return ((AndroidApplication) getApplication()).getQuestionRepositoryComponent();
