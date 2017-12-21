1fc540630c0f332671bdd7c9f8e94ae891ea7139
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-12-22 00:03:18 +0900

    Add local source tests.

---
+++ app/src/test/java/com/quangnguyen/stackoverflowclient/data/repository/local/QuestionLocalDataSourceTest.java
@@ -0,0 +1,50 @@
+package com.quangnguyen.stackoverflowclient.data.repository.local;
+
+import com.quangnguyen.stackoverflowclient.data.database.QuestionDao;
+import com.quangnguyen.stackoverflowclient.data.model.Question;
+import io.reactivex.Flowable;
+import io.reactivex.subscribers.TestSubscriber;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+public class QuestionLocalDataSourceTest {
+
+  @Mock QuestionDao questionDao;
+
+  private QuestionLocalDataSource localDataSource;
+
+  @Before public void setup() {
+    MockitoAnnotations.initMocks(this);
+    localDataSource = new QuestionLocalDataSource(questionDao);
+  }
+
+  @Test public void loadQuestions_ShouldReturnFromDatabase() {
+    List<Question> questions = Arrays.asList(new Question(), new Question());
+    TestSubscriber<List<Question>> subscriber = new TestSubscriber<>();
+    doReturn(Flowable.just(questions)).when(questionDao).getAllQuestions();
+
+    localDataSource.loadQuestions(false).subscribe(subscriber);
+
+    verify(questionDao).getAllQuestions();
+  }
+
+  @Test() public void addQuestion_ShouldInsertToDatabase() {
+    Question question = new Question();
+    localDataSource.addQuestion(question);
+
+    verify(questionDao).insert(question);
+  }
+
+  @Test() public void clearData_ShouldDeleteAllDataInDatabase() {
+    localDataSource.clearData();
+
+    verify(questionDao).deleteAll();
+  }
+}
